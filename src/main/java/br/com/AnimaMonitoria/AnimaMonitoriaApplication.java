abd4924133cf11616451d6495520fae3484432c0
Davi Pereira <davipereiraneto71@outlook.com>
2023-07-12 22:00:07 -0300

    Layout Inicial Projeto

---
+++ src/main/java/br/com/AnimaMonitoria/AnimaMonitoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.AnimaMonitoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaMonitoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaMonitoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/AnimaMonitoria/AnimaMonitoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.AnimaMonitoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimaMonitoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
